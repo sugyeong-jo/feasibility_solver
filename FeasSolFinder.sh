de9b783307f9d0dae725ba985ace12c05b54131c
sugyeong-jo <jclara811@gmail.com>
2020-05-28 15:15:29 +0900

    0528_result save

---
+++ FeasSolFinder.sh
@@ -0,0 +1,8 @@
+julia FeasSolFinder.jl R100701001_2_cplex.mps
+julia FeasSolFinder.jl R100701002_2_cplex.mps
+julia FeasSolFinder.jl R100701003_2_cplex.mps
+julia FeasSolFinder.jl R100701004_2_cplex.mps
+julia FeasSolFinder.jl R100701006_2_cplex.mps
+julia FeasSolFinder.jl R100701007_2_cplex.mps
+julia FeasSolFinder.jl R100701008_2_cplex.mps
+julia FeasSolFinder.jl R100701009_2_cplex.mps
